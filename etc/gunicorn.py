b043a865242841e4ecdff1f1635ced53bc26610b
godor1333 <godor1333@mail.ru>
2019-08-08 17:48:43 +0300

    asdf2.3

--- etc/gunicorn.py
+++ etc/gunicorn.py
@@ -1,14 +1,14 @@
-# CONFIG = {
-#     'mode': 'wsgi',
-#     'working_dir': '/home/box/web',
-#     'python': '/usr/bin/python',
-#     'args': (
-#         '--bind=0.0.0.0:8080',
-#         '--workers=2',
-#         '--timeout=60',
-#         '--daemon',
-#         'hello:application',
-#     ),
-# }
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web',
+    'python': '/usr/bin/python',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=2',
+        '--timeout=60',
+        '--daemon',
+        'hello:application',
+    ),
+}
 
-bind='0.0.0.0:8080'
+#bind='0.0.0.0:8080'
