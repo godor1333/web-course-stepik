267cbf8ac989fd4550d82690fccddd22e86bb946
godor1333 <godor1333@mail.ru>
2019-08-11 13:14:18 +0300

    final2.1

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/dj_conf.py /etc/gunicorn.d/dj_conf.py
-#gunicorn -c /etc/gunicorn.d/hello.py hello:app
+#gunicorn -c /etc/gunicorn.d/hello.py hello:app qwr
 gunicorn -c /etc/gunicorn.d/dj_conf.py ask.wsgi:application
